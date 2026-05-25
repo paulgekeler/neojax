8aaaf4d41e6fefc3af467a8e6c0f26c71a4ebfaa
Paul <pgekeler@gmail.com>
2026-05-25 14:59:58 +0200

    feat(losses): Import loss classes for exposure under neojax.losses for a flatter, cleaner API

---
+++ neojax/losses/__init__.py
@@ -0,0 +1,6 @@
+from neojax.losses.base_loss import BaseLoss as BaseLoss
+from neojax.losses.composed_loss import ComposedLoss as ComposedLoss
+from neojax.losses.lp_losses import LpLoss as LpLoss
+from neojax.losses.lp_losses import RelativeLpLoss as RelativeLpLoss
+from neojax.losses.sobolev_losses import H1Loss as H1Loss
+from neojax.losses.sobolev_losses import SobolevLoss as SobolevLoss

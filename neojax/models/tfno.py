773824489ab4f7d991d6ffd1a0503434ed950b7a
Paul <pgekeler@gmail.com>
2026-06-12 11:54:50 +0200

    feat(models): add Tucker factorized FNO

---
+++ neojax/models/tfno.py
@@ -0,0 +1,227 @@
+"""Implementation of a n-dimensional Tucker factorized FNO."""
+
+from collections.abc import Callable, Sequence
+from typing import Literal
+
+import equinox as eqx
+import jax
+import jax.random as jr
+from jaxtyping import Array, Float, PRNGKeyArray
+
+from neojax.nn.domain_padding import DomainPadding
+from neojax.nn.pointwise_mlp import PointwiseMLP
+from neojax.nn.positional_embedding import GridEmbeddingNd
+from neojax.nn.tfno_blocks import TFNOBlocks
+
+
+class TFNO(eqx.Module):
+    """General n-dimensional Tucker factorized FNO.
+
+    The model consists of a lifting layer that maps
+    the input to a higher-dimensional latent space,
+    a sequence of TFNO blocks (spectral convolutions + skip connections)
+    optionally interleaved with pointwise MLPs,
+    and a final projection layer.
+
+    Args:
+        key: PRNG key for parameter initialization.
+        in_channels: Number of input channels
+            (e.g., coordinates + initial conditions).
+        out_channels: Number of output channels (e.g., solution field).
+        hidden_channels: Hidden channel dimension (latent width)
+            used throughout the TFNO blocks. This significantly affects
+            the number of parameters. Good starting point is 64 and
+            increase if needed. Update `lift_channel_ratio` and
+            `proj_channel_ratio` accordingly.
+            They scale proportional to hidden_channels.
+        n_layers: Number of consecutive TFNO blocks.
+        modes: Number of Fourier modes to retain
+            across each spatial dimension.
+        ranks: Number of ranks to contract the spectral tensors to.
+            If `ranks` is an Integer, the same number is used
+            for all ranks. If not,
+            should be num_spatial_dims + num_channel_dims ranks,
+            e.g. in 2D 4 ranks (out_channel, in_channel, 2 spatial ranks).
+        share_factor_matrices: Whether to share the factor matrices
+            across the weight tensors. This further decreases the number
+            of weight tensors. Only the core tensor is not shared then.
+            Default is True.
+        activation: Activation function used within the layers.
+            Defaults to `jax.nn.gelu`.
+        use_channel_mlp: Whether to apply a pointwise channel MLP
+            after each TFNO block. Defaults to `True`.
+        local_operator: Type of skip connection inside TFNO blocks.
+            Can be `"linear"`, `"soft-gating"`, `"identity"`, or None.
+            Defaults to `"linear"`.
+        use_local_operator_bias: Whether to use a bias term
+            in the FNO blocks local operator. Default is `False`.
+        channel_mlp_residual: Type of skip connection around channel MLPs.
+            Can be `"linear"`, `"soft-gating"`, `"identity"`, or None.
+            Defaults to `"soft-gating"`.
+        channel_mlp_expansion: Expansion factor for hidden dimension
+            in the channel MLPs. Defaults to 0.5.
+        channel_mlp_activations: Activation function or sequence
+            of activation functions used inside the channel MLPs.
+            Default is `jax.nn.gelu`.
+        normalization: Type of normalization to use in TFNO blocks.
+            Can be `"layer"`, `"instance"`, `"group"` or None.
+            Default is `"layer"`.
+        norm_groups: Number of groups for group normalization.
+            Default is 1.
+        use_fno_residual: Whether to use residual connection
+            around TFNO blocks. Default is True.
+        preactivation: Whether to use pre-activation style blocks.
+            Default is False.
+        n_lift_layers: Number of layers in the lifting MLP.
+            Defaults to 2.
+        n_proj_layers: Number of layers in the projection MLP.
+            Defaults to 2.
+        lift_channel_ratio: Ratio of lifting channels
+            to hidden_channels.
+            The number of lifting channels in the lifting block
+            of the TFNO is lifting_channel_ratio * hidden_channels
+            (e.g. default 2 * hidden_channels).
+        proj_channel_ratio: Ratio of projection channels
+            to hidden_channels.
+            The number of projection channels in the projection block
+            of the TFNO is projection_channel_ratio * hidden_channels
+            (e.g. default 2 * hidden_channels).
+        positional_embedding: Positional embedding to apply
+            to last channels of raw input before passing through TFNO.
+            Defaults to regular GridEmbeddingNd on a ((0, 1), ...) grid.
+        domain_padding: Percentage of padding to use.
+            If single float, this padding is used for all dims.
+            Sequence of floats indicates padding percentage per dim.
+            Default is None, no padding.
+
+    !!! info "Internal Attributes"
+        These fields store the internal layers state (and weights).
+
+        * **positional_embedding** (`GridEmbeddingNd | None`): Positional embedding to apply to last channels of raw input before passing through TFNO.
+        * **lifting** (`PointwiseMLP`): The `PointwiseMLP` used to lift inputs to the hidden `hidden_channels`.
+        * **tfno_blocks** (`TFNOBlocks`): The `TFNOBlocks` sequence containing spectral convolutions.
+        * **projection** (`PointwiseMLP`): The `PointwiseMLP` used to project latent features to `out_channels`.
+        * **padding** (`DomainPadding | None`): Percentage of domain padding to use.
+
+    !!! info
+        The TFNO has the same architecture as the FNO except for the
+        factorized spectral weights.
+
+    !!! info "Upcoming Features"
+        Current implementation doesn't support different TFNO block precisions,
+        resolution scaling, stabilizers,
+        separable spectral convolutions or enforcing hermitian symmetry.
+        These will be added in future releases.
+    """
+
+    positional_embedding: GridEmbeddingNd | None
+    lifting: PointwiseMLP
+    tfno_blocks: TFNOBlocks
+    projection: PointwiseMLP
+    padding: DomainPadding | None
+
+    def __init__(
+        self,
+        key: PRNGKeyArray,
+        in_channels: int,
+        out_channels: int,
+        hidden_channels: int,
+        n_layers: int,
+        modes: int | Sequence[int],
+        ranks: int | Sequence[int],
+        share_factor_matrices: bool = True,
+        activation: Callable = jax.nn.gelu,
+        use_channel_mlp: bool = True,
+        local_operator: Literal["linear", "soft-gating", "identity"] | None = "linear",
+        use_local_operator_bias: bool = False,
+        channel_mlp_residual: Literal["linear", "soft-gating", "identity"]
+        | None = "soft-gating",
+        channel_mlp_expansion: float | None = 0.5,
+        channel_mlp_activations: Callable | Sequence[Callable] = jax.nn.gelu,
+        normalization: Literal["layer", "instance", "group"] | None = "layer",
+        norm_groups: int = 1,
+        use_fno_residual: bool = True,
+        preactivation: bool = False,
+        n_lift_layers: int = 2,
+        n_proj_layers: int = 2,
+        lift_channel_ratio: float = 2.0,
+        proj_channel_ratio: float = 2.0,
+        positional_embedding: GridEmbeddingNd | None = None,
+        domain_padding: float | Sequence[float] | None = None,
+    ) -> None:
+        lkey, pkey, tfno_key = jr.split(key, 3)
+
+        self.positional_embedding = positional_embedding
+        if domain_padding is not None:
+            self.padding = DomainPadding(domain_padding)
+        else:
+            self.padding = None
+
+        lift_hidden_channels = int(hidden_channels * lift_channel_ratio)
+        lifting_layers = (
+            [in_channels]
+            + [lift_hidden_channels] * (n_lift_layers - 1)
+            + [hidden_channels]
+        )
+        self.lifting = PointwiseMLP(
+            key=lkey,
+            layers=tuple(lifting_layers),
+            activations=activation,
+        )
+        proj_hidden_channels = int(hidden_channels * proj_channel_ratio)
+        projection_layers = (
+            [hidden_channels]
+            + [proj_hidden_channels] * (n_proj_layers - 1)
+            + [out_channels]
+        )
+        self.projection = PointwiseMLP(
+            key=pkey,
+            layers=tuple(projection_layers),
+            activations=activation,
+        )
+        self.tfno_blocks = TFNOBlocks(
+            key=tfno_key,
+            n_layers=n_layers,
+            in_channels=hidden_channels,
+            out_channels=hidden_channels,
+            modes=modes,
+            ranks=ranks,
+            share_factor_matrices=share_factor_matrices,
+            activation=activation,
+            use_channel_mlp=use_channel_mlp,
+            preactivation=preactivation,
+            normalization=normalization,
+            norm_groups=norm_groups,
+            use_fno_residual=use_fno_residual,
+            local_operator=local_operator,
+            use_local_operator_bias=use_local_operator_bias,
+            channel_mlp_residual=channel_mlp_residual,
+            channel_mlp_expansion=channel_mlp_expansion,
+            channel_mlp_activations=channel_mlp_activations,
+        )
+
+    def __call__(self, x: Float[Array, "in_c ..."]) -> Float[Array, "out_c ..."]:
+        """Forward pass of the TFNO model.
+
+        Args:
+            x: Input array of shape `(in_channels, d1, ..., dn)`,
+                where `n` matches the dimensionality of `modes`.
+
+        Returns:
+            The predicted field of shape `(out_channels, d1, ..., dn)`.
+        """
+        if self.positional_embedding is not None:
+            x = self.positional_embedding(x)
+
+        original_shape = x.shape
+        if self.padding is not None:
+            x = self.padding.pad(x)
+
+        x = self.lifting(x)
+        x = self.tfno_blocks(x)
+        x = self.projection(x)
+
+        if self.padding is not None:
+            x = self.padding.unpad(x, original_shape)
+
+        return x

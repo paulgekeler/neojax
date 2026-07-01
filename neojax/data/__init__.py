11f9938276ac5bae513f75fefe675f5f534bb840
Paul <pgekeler@gmail.com>
2026-07-01 14:22:37 +0200

    docs: ruff format data init-

--- neojax/data/__init__.py
+++ neojax/data/__init__.py
@@ -1,6 +1,6 @@
 """Data utilities for neural operators."""
 
-from neojax.data import normalizers, scales, download
+from neojax.data import download, normalizers, scales
 from neojax.data.download.dataset import download_dataset
 
 __all__ = ["normalizers", "scales", "download", "download_dataset"]

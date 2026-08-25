1050004ecdbf96e1aafe1aeba112b0706dfe2e70
Paul <pgekeler@gmail.com>
2026-08-25 11:33:38 +0200

    fix: Add MLPDeepONet and GeoFNO to init, fix imports

--- neojax/models/__init__.py
+++ neojax/models/__init__.py
@@ -1,13 +1,19 @@
 """Models for neural operators."""
 
-from neojax.models.baseno import BaseNO
-from neojax.models.deeponet import DeepONet
-from neojax.models.fno import FNO
-from neojax.models.tfno import TFNO
+from neojax.models.baseno import BaseNO as BaseNO
+from neojax.models.deeponet import DeepONet as DeepONet
+from neojax.models.deeponet import MLPDeepONet as MLPDeepONet
+from neojax.models.fno import FNO as FNO
+from neojax.models.geo_fno import GeoFNO as GeoFNO
+from neojax.models.tfno import TFNO as TFNO
+from neojax.models.uno import UNO as UNO
 
 __all__ = [
     "DeepONet",
+    "MLPDeepONet",
     "FNO",
+    "GeoFNO",
     "TFNO",
+    "UNO",
     "BaseNO",
 ]

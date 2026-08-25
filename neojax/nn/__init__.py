a03cc73f49b457c91200cca3b4a4b64b3d087801
Paul <pgekeler@gmail.com>
2026-08-25 10:49:50 +0200

    format: Format with ruff

--- neojax/nn/__init__.py
+++ neojax/nn/__init__.py
@@ -1,23 +1,31 @@
 """Neural network layers and blocks for neural operators."""
 
-from neojax.nn.domain_padding import DomainPadding
-from neojax.nn.fno_blocks import FNOBlock, FNOBlocks
-from neojax.nn.normalization import InstanceNorm
-from neojax.nn.pointwise_mlp import PointwiseMLP
-from neojax.nn.positional_embedding import GridEmbeddingNd
-from neojax.nn.resample import Resampler
-from neojax.nn.skip_connections import Flattened1dConv, SoftGating
-from neojax.nn.spectral_conv import SpectralConvNd
+from neojax.nn.domain_padding import DomainPadding as DomainPadding
+from neojax.nn.fno_blocks import FNOBlock as FNOBlock
+from neojax.nn.fno_blocks import FNOBlocks as FNOBlocks
+from neojax.nn.geo_map import GeoMapNd as GeoMapNd
+from neojax.nn.geo_spectral_conv import GeoSpectralConvNd as GeoSpectralConvNd
+from neojax.nn.normalization import InstanceNorm as InstanceNorm
+from neojax.nn.pointwise_mlp import PointwiseMLP as PointwiseMLP
+from neojax.nn.positional_embedding import GridEmbeddingNd as GridEmbeddingNd
+from neojax.nn.resample import Resampler as Resampler
+from neojax.nn.skip_connections import Flattened1dConv as Flattened1dConv
+from neojax.nn.skip_connections import SoftGating as SoftGating
+from neojax.nn.skip_connections import make_skip_connection as make_skip_connection
+from neojax.nn.spectral_conv import SpectralConvNd as SpectralConvNd
 
 __all__ = [
     "DomainPadding",
     "FNOBlock",
     "FNOBlocks",
+    "GeoMapNd",
+    "GeoSpectralConvNd",
     "InstanceNorm",
     "PointwiseMLP",
     "GridEmbeddingNd",
     "Flattened1dConv",
     "SoftGating",
+    "make_skip_connection",
     "SpectralConvNd",
     "Resampler",
 ]
